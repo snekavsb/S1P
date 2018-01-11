bd01d3cc9293813358261cb6e5d84270e6835ea1
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-11 10:45:21 +0000

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf(a is greater");
+}
+else
+{
+print("c is greater");
+}
+else
+}
+}
