9fd984914cda961c4d54e52b335a057e04df66b3
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-12 10:26:34 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int N[5]={'1','2','3','4','5'},k,sum=0;
+	
+	
+	for(k=1;k<=2;k++)
+	{
+		printf("%d\n",k);
+		sum= sum+k;
+	}
+	printf("%d\n",sum);
+	return 0;
+}
