9a174b5b22b6b24a3be29ad79cd6e8c95ae158aa
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-12 08:07:00 +0530

    Create alphabet\not.c

---
+++ alphabet\not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	char ch='9';
+	if((ch>='a'&&ch<='z')||(ch>='A' && ch<='Z'))
+	{
+		printf("alphabet");
+		
+	}
+	else
+	{
+		printf("not alphabet");
+	}
+	
+	return 0;
+}
